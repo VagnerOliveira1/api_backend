8afa8fc02f11c527fec9880fe998637e5ce762b6
Vagner Oliveira <ti.vagner@gmail.com>
2021-03-03 09:44:38 -0300

    Configura teste de Request

---
+++ spec/requests/admin/v1/home_spec.rb
@@ -0,0 +1,16 @@
+require  'rails_helper'
+
+describe "Home", type: :request do
+  let(:user) {create(:user)}
+
+  it "Teste Home" do
+    get '/admin/v1/home', headers: auth_header(user)
+    expect(body_json).to eq({'message' => 'Teste'})
+  end
+
+  it "Teste Home" do
+    get '/admin/v1/home', headers: auth_header(user)
+    expect(response).to have_http_status(:ok)
+  end
+end
+
