0af1702d59cd5034036a2c9c25c6faa3ff106fea
Vagner Oliveira <ti.vagner@gmail.com>
2021-03-02 21:10:23 -0300

    Cria validação Customizada

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module EcommerceApi
     config.i18n.default_locale = :'pt-BR'
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
 
+    config.autoload_paths += %W["#{config.root}/app/validators/"]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
