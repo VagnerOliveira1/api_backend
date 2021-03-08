f85d6db392dc0ca9787731a64b20d5d674934bb1
Vagner Oliveira <ti.vagner@gmail.com>
2021-03-08 16:45:17 -0300

    Cria SystemRequirements e renderiza erro

--- spec/requests/admin/v1/system_requirements/admin_request_spec.rb
+++ spec/requests/admin/v1/system_requirements/admin_request_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe "Admin::V1::SystemRequirements", type: :request do
 
     it "return all System Requirements" do
       get url, headers: auth_header(user)
-      p body_json
       expect(body_json['system_requirements']).to contain_exactly *system_requirements.as_json(only: %i(id name))
     end
 
@@ -19,5 +18,53 @@ RSpec.describe "Admin::V1::SystemRequirements", type: :request do
     end
 
   end
+
+  context "POST /system_requirements" do
+    let(:url) {"/admin/v1/system_requirements"}
+
+    context "with valid params" do
+      let(:system_requirement_params) {{ system_requirement: attributes_for(:system_requirement)}.to_json}
+
+      it "adds a new SystemRequirements" do
+        expect do
+          post url, headers: auth_header(user), params: system_requirement_params
+        end.to change(SystemRequirement, :count).by(1)
+      end
+
+      it "return last added SystemRequirements" do
+        post url, headers: auth_header(user), params: system_requirement_params
+        expect_system_requirement = SystemRequirement.last.as_json(only: %i(id name operational_system storage))
+        expect(body_json['system_requirement']).to eq expect_system_requirement
+      end
+
+      it "returns success status" do
+        post url, headers: auth_header(user), params: system_requirement_params
+        expect(response).to have_http_status(:ok)
+      end
+
+    end
+    context "with invalid params" do
+      let(:system_requirement_invalid_params) do
+        {system_requirement: attributes_for(:system_requirement, name: nil)}.to_json
+      end
+
+      it "does not add a new SystemRequirements" do
+        expect do
+          post url, headers: auth_header(user), params: system_requirement_invalid_params
+        end.to_not change(SystemRequirement, :count)
+      end
+
+      it "returns error messages" do
+        post url, headers: auth_header(user), params: system_requirement_invalid_params
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it "returns unprocessable_entity status" do
+        post url, headers: auth_header(user),params: system_requirement_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
 
 end
