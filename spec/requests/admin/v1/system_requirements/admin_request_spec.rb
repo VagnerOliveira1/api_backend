91a7ac3820430a464d6633ac4c2d9ee55d5770e5
Vagner Oliveira <ti.vagner@gmail.com>
2021-03-09 15:09:20 -0300

    Atualiza e remove SystemRequirements

--- spec/requests/admin/v1/system_requirements/admin_request_spec.rb
+++ spec/requests/admin/v1/system_requirements/admin_request_spec.rb
@@ -65,6 +65,85 @@ RSpec.describe "Admin::V1::SystemRequirements", type: :request do
       end
     end
   end
+  context "PATCH /system_requirements/:id" do
+    let(:system_requirement) {create(:system_requirement)}
+    let(:url) {"/admin/v1/system_requirements/#{system_requirement.id}"}
 
+    context "with valid params" do
+      let(:new_name) { "My new System" }
+      let(:system_requirement_params) {{system_requirement: {name: new_name}}.to_json}
+
+      it "updates SystemRequirements" do
+        patch url, headers: auth_header(user), params: system_requirement_params
+        system_requirement.reload
+        expect(system_requirement.name).to eq new_name
+      end
+
+      it "returns update category" do
+        patch url, headers: auth_header(user), params: system_requirement_params
+        system_requirement.reload
+        expect_system_requirement = system_requirement.as_json(only: %i(id name operational_system storage))
+        expect(body_json['system_requirement']).to eq expect_system_requirement
+      end
+
+      it "returns success status" do
+        patch url, headers: auth_header(user), params: system_requirement_params
+        expect(response).to have_http_status(:ok)
+      end
+
+    end
+
+    context "with invalid params" do
+      let(:system_requirement_invalid_params) do
+        {system_requirement: attributes_for(:system_requirement, name: nil)}.to_json
+      end
+      it "Does not update SystemRequirement" do
+        old_name = system_requirement.name
+        patch url, headers: auth_header(user), params: system_requirement_invalid_params
+        system_requirement.reload
+        expect(system_requirement.name).to eq old_name
+      end
+      it "returns error messages" do
+        patch url, headers: auth_header(user), params: system_requirement_invalid_params
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it "returns unprocessable_entity status" do
+        patch url, headers: auth_header(user), params: system_requirement_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+
+  context "DELETE /system_requirements/:id" do
+    let!(:system_requirement) {create(:system_requirement)}
+    let(:url){"/admin/v1/system_requirements/#{system_requirement.id}"}
+
+    it "removes SystemRequirement" do
+      expect do
+        delete url, headers: auth_header(user)
+      end.to change(SystemRequirement, :count).by(-1)
+    end
+
+    it "returns success status" do
+      delete url, headers: auth_header(user)
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it "does not return any body content" do
+      delete url, headers: auth_header(user)
+      expect(body_json).to_not be_present
+    end
+
+    # it "removes all associated Games" do
+    #   games = create_list(:game, 3, system_requirement: system_requirement)
+    #   delete url, headers: auth_header(user)
+    #   expected_games = Game.where(id: games.map(&:id))
+    #   p expected_games
+    #   expect(expected_games).to eq []
+    # end
+
+  end
 
 end
